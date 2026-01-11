c96676130edf05bd63e65f72f2c693e0db62ad21
MeiQi_Wa <mqleow20@gmail.com>
2026-01-11 11:45:39 +0800

    ending ques

---
+++ Assets/_MQ_Asset/MQ_Script/AnswerButton.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public bool isCorrect;
+
+    public Color correctColor = Color.green;
+    public Color wrongColor = Color.red;
+
+    private Image image;
+    private Color normalColor;
+
+    private static bool isLocked = false;
+    private QuizManager quizManager;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        normalColor = image.color;
+        quizManager = FindObjectOfType<QuizManager>();
+
+        GetComponent<Button>().onClick.AddListener(OnAnswerClicked);
+    }
+
+    void OnAnswerClicked()
+    {
+        if (isLocked) return;
+        StartCoroutine(HandleAnswer());
+    }
+
+    IEnumerator HandleAnswer()
+    {
+        isLocked = true;
+        LockAllButtons(true);
+
+        image.color = isCorrect ? correctColor : wrongColor;
+
+        yield return new WaitForSeconds(5f);
+
+        if (isCorrect)
+        {
+            quizManager.CorrectAnswer();
+        }
+        else
+        {
+            image.color = normalColor;
+            isLocked = false;
+            LockAllButtons(false);
+        }
+    }
+
+    void LockAllButtons(bool lockState)
+    {
+        AnswerButton[] buttons = FindObjectsOfType<AnswerButton>();
+        foreach (AnswerButton btn in buttons)
+        {
+            btn.GetComponent<Button>().interactable = !lockState;
+        }
+    }
+
+    public static void ResetLock()
+    {
+        isLocked = false;
+    }
+
+}
---
+++ Assets/_MQ_Asset/MQ_Script/QuizManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    public GameObject[] questions;
+
+    private int currentQuestionIndex = 0;
+
+    void Start()
+    {
+        ShowQuestion(0);
+    }
+
+    public void CorrectAnswer()
+    {
+        AnswerButton.ResetLock();
+        NextQuestion();
+    }
+
+
+    void NextQuestion()
+    {
+        questions[currentQuestionIndex].SetActive(false);
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.Length)
+        {
+            questions[currentQuestionIndex].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Quiz Finished!");
+            Destroy(gameObject);
+        }
+    }
+
+    void ShowQuestion(int index)
+    {
+        for (int i = 0; i < questions.Length; i++)
+        {
+            questions[i].SetActive(i == index);
+        }
+    }
+}
