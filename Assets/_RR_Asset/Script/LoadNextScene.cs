873497a50ddf43f3d858bc703776503d8980be6c
RRTeoh <rrteoh1103@gmail.com>
2026-01-12 09:05:40 +0800

    loadscene

--- Assets/_RR_Asset/Script/LoadNextScene.cs
+++ Assets/_RR_Asset/Script/LoadNextScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadScene : MonoBehaviour
+public class LoadNextScene : MonoBehaviour
 {
     public string sceneName;
     // Start is called before the first frame update
