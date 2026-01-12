28f4f02881f938713a997dc323f1e1c3d3a82454
MeiQi_Wa <mqleow20@gmail.com>
2026-01-12 21:44:42 +0800

    mq

--- Assets/_SC_Assets/SC_Scripts/NewScript/BGM.cs
+++ Assets/_SC_Assets/SC_Scripts/NewScript/BGM.cs
@@ -21,6 +21,7 @@ public class BGM : MonoBehaviour
         if (bgmSource != null && !bgmSource.isPlaying)
         {
             bgmSource.Play();
+            Debug.Log("bgm played");
         }
     }
 
