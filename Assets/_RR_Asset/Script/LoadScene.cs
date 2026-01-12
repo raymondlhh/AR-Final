7aae48e84334470cd730299db6916879074b3174
RRTeoh <rrteoh1103@gmail.com>
2026-01-12 09:00:56 +0800

    loadscene

--- Assets/_RR_Asset/Script/LoadScene.cs
+++ Assets/_RR_Asset/Script/LoadScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
+    public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,6 @@ public class LoadScene : MonoBehaviour
 
     public void OnEnable()
     {
-        SceneManager.LoadScene("EndingScene");
+        SceneManager.LoadScene(sceneName);
     }
 }
